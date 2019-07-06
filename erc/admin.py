1dbb035317780029c88fa6e06517b782e60c2010
Rohan <Samrat@mac.local>
2019-07-07 00:35:53 +0530

    created basic models

--- erc/admin.py
+++ erc/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Event,BlogArticle,BlogPic
 # Register your models here.
+
+admin.site.register(Event)
+admin.site.register(BlogArticle)
+admin.site.register(BlogPic)

15e7aa656ca7f8f549da9ad58ae563019536f382
Rohan <Samrat@mac.local>
2019-07-16 05:23:09 +0530

    merging events.

--- erc/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Event,Blog,BlogPart,BlogPic
-# Register your models here.
-
-admin.site.register(Event)
-admin.site.register(Blog)
-admin.site.register(BlogPart)
-admin.site.register(BlogPic)
--- erc/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ErcConfig(AppConfig):
-    name = 'erc'

c0e576ee08a18a98f72429bc29e31ce09bcb3a9c
samarthdoshi2306 <samarthchotu2@gmail.com>
2019-07-16 03:16:54 +0530

    edited models and added event images to database

--- erc/admin.py
+++ erc/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Event,BlogArticle,BlogPic
+from .models import Event, Blog, BlogPart, BlogPic
+
 # Register your models here.
 
 admin.site.register(Event)
-admin.site.register(BlogArticle)
+admin.site.register(Blog)
+admin.site.register(BlogPart)
 admin.site.register(BlogPic)

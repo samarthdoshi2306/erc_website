0a4ee983781c0b1938b6a0e3f600871181c43685
samarthdoshi2306 <samarthchotu2@gmail.com>
2019-07-18 20:42:51 +0530

    edited views

--- erc/views.py
+++ erc/views.py
@@ -1,4 +1,13 @@
+from django.http import Http404
 from django.shortcuts import render
+from erc.models import Event
 # Create your views here.
 def home_view (request, *args, **kwargs):
 	return render(request,"erc/index.html",{})
+
+def event_detail(request,Event_id):
+	try:
+		event=Event.objects.get(pk=Event_id)
+	except Event.DoesNotExist:
+		raise Http404("Event Does Not Exist")
+	return render(request,'erc/event_detail.html',{'event':event})

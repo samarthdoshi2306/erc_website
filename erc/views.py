3dd30b9c8e8cd3544568b45951097e62c0eb3521
Rohan <Samrat@mac.local>
2019-07-19 05:26:54 +0530

    finished teams

--- erc/views.py
+++ erc/views.py
@@ -1,13 +1,15 @@
 from django.http import Http404
 from django.shortcuts import render
 from .models import *
+from django.db import connections
+
 # Create your views here.
 def home_view (request, *args, **kwargs):
-	team=Team.objects.get(id=1)
-	content={
-		'team':team,
-	}
-	return render(request,"erc/index.html",content)
+	anirudh=Team.objects.get(id=2)
+	jian=Team.objects.get(id=3)
+	queryset=Team.objects.all()
+
+	return render(request,"erc/index.html",{'anirudh':anirudh,'jian':jian,'team':queryset })
 
 def event_detail(request,Event_id):
 	try:
